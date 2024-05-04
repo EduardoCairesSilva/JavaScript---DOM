757122f2912fc25e81b0ecacdb44baa6e244a303
Eduardo Caires <yandudu743@gmail.com>
2024-05-04 11:10:44 -0300

    Retirando o else do código e adicionando forma de early return

--- form-validate/script.js
+++ form-validate/script.js
@@ -1,21 +1,22 @@
 let inputEmail = document.getElementById("input-email");
 let inputSenha = document.getElementById("input-senha");
 let formLogin = document.getElementById("form-login");
-let emailError = document.getElementById("email-error")
-let senhaError = document.getElementById("senha-error")
+let emailError = document.getElementById("email-error");
+let senhaError = document.getElementById("senha-error");
 
 formLogin.addEventListener("submit", function (event) {
-    
-    let emailLength = inputEmail.value.length
-    let senhaLength = inputSenha.value.length
+  let emailLength = inputEmail.value.length;
+  let senhaLength = inputSenha.value.length;
+  emailError.innerHTML = "";
+  senhaError.innerHTML = "";
+
+  if (!emailLength) {
+    emailError.innerHTML = "<span>Email precisa ser preenchido</span>";
     event.preventDefault();
+  }
 
-    if(!emailLength) {
-        emailError.innerHTML = "<span>Email precisa ser preenchido</span>"
-    }
-
-    if(!senhaLength) {
-        senhaError.innerHTML = "<span>Senha precisa ser preenchida</span>"
-    }
-
+  if (!senhaLength) {
+    senhaError.innerHTML = "<span>Senha precisa ser preenchida</span>";
+    event.preventDefault();
+  }
 });
